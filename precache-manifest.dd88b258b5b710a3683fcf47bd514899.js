29ec1421529130fd32a3d4fadb29c27c31ecef0c
Simon Mändle <simon.maendle@outlook.com>
2021-09-21 21:50:00 +0200

    Updates

---
+++ precache-manifest.dd88b258b5b710a3683fcf47bd514899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7fcc301d2ca6835e0de87e4e15ddb03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e38c80a46d8e4827d925",
+    "url": "./static/css/main.6349cca6.chunk.css"
+  },
+  {
+    "revision": "ef71fc4078566849797a",
+    "url": "./static/js/2.08826b83.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.08826b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38c80a46d8e4827d925",
+    "url": "./static/js/main.30616978.chunk.js"
+  },
+  {
+    "revision": "78482aaa971910735a0f",
+    "url": "./static/js/runtime-main.7dae86bb.js"
+  },
+  {
+    "revision": "4a5ab6bdf8ec97181288d3a27ad6d4e4",
+    "url": "./static/media/chromecast~bhycLeeK.4a5ab6bd.svg"
+  }
+]);
